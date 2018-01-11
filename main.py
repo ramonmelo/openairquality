df3c62c69a3ffc3ff104a81a9fd51c76bed82a59
Ramon Melo <me@ramonmelo.com>
2018-01-11 14:39:20 -0300

    add temperature and humidity sensor support

--- main.py
+++ main.py
@@ -2,8 +2,26 @@
 # @Author: Ramon Melo
 # @Date:   2018-01-09
 # @Last Modified by:   Ramon Melo
-# @Last Modified time: 2018-01-09
+# @Last Modified time: 2018-01-10
 
-from src import app
+from thsensor import THSensor
+import time
+from machine import Pin, I2C
 
-app.run()
+# construct an I2C bus
+i2c = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
+
+i2c.readfrom(0x3a, 4)   # read 4 bytes from slave device with address 0x3a
+
+# i2c.writeto(0x3a, '12') # write '12' to slave device with address 0x3a
+
+# buf = bytearray(10)     # create a buffer with 10 bytes
+# i2c.writeto(0x3a, buf)  # write the given buffer to the slave
+
+sensor = THSensor(2)
+
+while True:
+    print(sensor.data())
+
+    time.sleep(1)
+
--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Ramon Melo
-# @Date:   2018-01-09
-# @Last Modified by:   Ramon Melo
-# @Last Modified time: 2018-01-09
